a1a166ec7c4ce7976f87174217b14fc874b86668
tanguy <tanguy.cazalets@protonmail.com>
2023-03-13 12:17:33 +0100

    Added code explainations and small changes

--- custom_env_step_numba.py
+++ custom_env_step_numba.py
@@ -26,22 +26,22 @@ def ComputeDistance(
 
 
 # Device helper function to generate observation
-@numba_driver.jit((float32[:, ::1],
-                   float32[:, ::1],
-                   float32[:, ::1],
-                   float32[:, ::1],
-                   float32[:, ::1],
-                   int32[::1],
-                   float32,
-                   float32,
-                   int32[:, ::1],
-                   boolean,
-                   float32[:, :, ::1],
-                   int32[::1],
-                   int32,
-                   int32,
-                   int32,
-                   int32),
+@numba_driver.jit((float32[:, ::1],  # loc_x_arr
+                   float32[:, ::1],  # loc_y_arr
+                   float32[:, ::1],  # speed_arr
+                   float32[:, ::1],  # direction_arr
+                   float32[:, ::1],  # acceleration_arr
+                   int32[::1],  # agent_types_arr
+                   float32,  # kGridLength
+                   float32,  # kMaxSpeed
+                   int32[:, ::1],  # still_in_the_game_arr
+                   boolean,  # kUseFullObservation
+                   float32[:, :, ::1],  # obs_arr
+                   int32[::1],  # env_timestep_arr
+                   int32,  # kNumAgents
+                   int32,  # kEpisodeLength
+                   int32,  # kEnvId
+                   int32),  # kThisAgentId
                   device=True)
 def CudaCustomEnvGenerateObservation(
         loc_x_arr,
@@ -54,84 +54,64 @@ def CudaCustomEnvGenerateObservation(
         kMaxSpeed,
         still_in_the_game_arr,
         kUseFullObservation,
-        obs_arr,  
+        obs_arr,
         env_timestep_arr,
         kNumAgents,
         kEpisodeLength,
         kEnvId,
         kThisAgentId,
 ):
+    # for each agent, we have 7 features that we can observe
     num_features = 7
 
     if kThisAgentId < kNumAgents:
         if kUseFullObservation:
-            # Initialize obs
-            index = 0
+            # create the array of other agents ids
+            other_agents_ids = [i for i in range(kNumAgents) if i != kThisAgentId]
 
-            for other_agent_id in range(kNumAgents):
-                if not other_agent_id == kThisAgentId:
-                    obs_arr[kEnvId, kThisAgentId, 0 * (kNumAgents - 1) + index] = 0.0
-                    obs_arr[kEnvId, kThisAgentId, 1 * (kNumAgents - 1) + index] = 0.0
-                    obs_arr[kEnvId, kThisAgentId, 2 * (kNumAgents - 1) + index] = 0.0
-                    obs_arr[kEnvId, kThisAgentId, 3 * (kNumAgents - 1) + index] = 0.0
-                    obs_arr[kEnvId, kThisAgentId, 4 * (kNumAgents - 1) + index] = 0.0
-                    obs_arr[kEnvId, kThisAgentId, 5 * (kNumAgents - 1) + index] = agent_types_arr[other_agent_id]
-                    obs_arr[kEnvId, kThisAgentId, 6 * (kNumAgents - 1) + index] = still_in_the_game_arr[kEnvId, other_agent_id]
-                    index += 1
+            # Initialize obs of other agents to 0 physics variable and to agent type and still in the game
+            for index, other_agent_id in enumerate(other_agents_ids):
+                obs_arr[kEnvId, kThisAgentId, 0 * (kNumAgents - 1) + index] = 0.0 # futur position of loc_x
+                obs_arr[kEnvId, kThisAgentId, 1 * (kNumAgents - 1) + index] = 0.0
+                obs_arr[kEnvId, kThisAgentId, 2 * (kNumAgents - 1) + index] = 0.0
+                obs_arr[kEnvId, kThisAgentId, 3 * (kNumAgents - 1) + index] = 0.0
+                obs_arr[kEnvId, kThisAgentId, 4 * (kNumAgents - 1) + index] = 0.0
+                obs_arr[kEnvId, kThisAgentId, 5 * (kNumAgents - 1) + index] = agent_types_arr[other_agent_id]
+                obs_arr[kEnvId, kThisAgentId, 6 * (kNumAgents - 1) + index] = still_in_the_game_arr[kEnvId, other_agent_id]
 
             obs_arr[kEnvId, kThisAgentId, num_features * (kNumAgents - 1)] = 0.0
 
             # Update obs for agents still in the game
             if still_in_the_game_arr[kEnvId, kThisAgentId]:
-                index = 0
-                for other_agent_id in range(kNumAgents):
-                    if not other_agent_id == kThisAgentId:
-                        obs_arr[
-                            kEnvId, kThisAgentId, 0 * (kNumAgents - 1) + index
-                        ] = float(
-                            loc_x_arr[kEnvId, other_agent_id]
-                            - loc_x_arr[kEnvId, kThisAgentId]
-                        ) / (
-                            math.sqrt(2.0) * kGridLength
-                        )
-                        obs_arr[
-                            kEnvId, kThisAgentId, 1 * (kNumAgents - 1) + index
-                        ] = float(
-                            loc_y_arr[kEnvId, other_agent_id]
-                            - loc_y_arr[kEnvId, kThisAgentId]
-                        ) / (
-                            math.sqrt(2.0) * kGridLength
-                        )
-                        obs_arr[
-                            kEnvId, kThisAgentId, 2 * (kNumAgents - 1) + index
-                        ] = float(
-                            speed_arr[kEnvId, other_agent_id]
-                            - speed_arr[kEnvId, kThisAgentId]
-                        ) / (
-                            kMaxSpeed + kEpsilon
-                        )
-                        obs_arr[
-                            kEnvId, kThisAgentId, 3 * (kNumAgents - 1) + index
-                        ] = float(
-                            acceleration_arr[kEnvId, other_agent_id]
-                            - acceleration_arr[kEnvId, kThisAgentId]
-                        ) / (
-                            kMaxSpeed + kEpsilon
-                        )
-                        obs_arr[kEnvId, kThisAgentId, 4 * (kNumAgents - 1) + index] = (
-                            float(
-                                direction_arr[kEnvId, other_agent_id]
-                                - direction_arr[kEnvId, kThisAgentId]
-                            )
-                            / kTwoPi
-                        )
-                        index += 1
+                # Update obs of other agents
+                for index, other_agent_id in enumerate(other_agents_ids):
+                    # update relative normalized pos_x of the other agent
+                    obs_arr[kEnvId, kThisAgentId, 0 * (kNumAgents - 1) + index] = float(
+                        loc_x_arr[kEnvId, other_agent_id] - loc_x_arr[kEnvId, kThisAgentId]
+                    ) / (math.sqrt(2.0) * kGridLength)
+                    # update relative normalized pos_y of the other agent
+                    obs_arr[kEnvId, kThisAgentId, 1 * (kNumAgents - 1) + index] = float(
+                        loc_y_arr[kEnvId, other_agent_id] - loc_y_arr[kEnvId, kThisAgentId]
+                    ) / (math.sqrt(2.0) * kGridLength)
+                    # update relative normalized speed of the other agent
+                    obs_arr[kEnvId, kThisAgentId, 2 * (kNumAgents - 1) + index] = float(
+                        speed_arr[kEnvId, other_agent_id] - speed_arr[kEnvId, kThisAgentId]
+                    ) / (kMaxSpeed + kEpsilon)
+                    # update relative normalized acceleration of the other agent
+                    obs_arr[kEnvId, kThisAgentId, 3 * (kNumAgents - 1) + index] = float(
+                        acceleration_arr[kEnvId, other_agent_id] - acceleration_arr[kEnvId, kThisAgentId]
+                    ) / (kMaxSpeed + kEpsilon)
+                    # update relative normalized direction of the other agent
+                    obs_arr[kEnvId, kThisAgentId, 4 * (kNumAgents - 1) + index] = float(
+                        direction_arr[kEnvId, other_agent_id] - direction_arr[kEnvId, kThisAgentId]
+                    ) / kTwoPi
 
+                # add the time remaining in the episode
                 obs_arr[kEnvId, kThisAgentId, num_features * (kNumAgents - 1)] = (
-                    float(env_timestep_arr[kEnvId]) / kEpisodeLength
+                        float(env_timestep_arr[kEnvId]) / kEpisodeLength
                 )
         else:
-            # Initialize obs to all zeros
+            # Set all obs to zeros if the agent is not in the game
             for idx in range(kNumAgents):
                 obs_arr[kEnvId, kThisAgentId, 0 * kNumAgents + idx] = 0.0
                 obs_arr[kEnvId, kThisAgentId, 1 * kNumAgents + idx] = 0.0
@@ -142,7 +122,6 @@ def CudaCustomEnvGenerateObservation(
                 obs_arr[kEnvId, kThisAgentId, 6 * kNumAgents + idx] = 0.0
 
             obs_arr[kEnvId, kThisAgentId, num_features * kNumAgents] = 0.0
-
 
 
 # Device helper function to compute rewards
@@ -166,24 +145,24 @@ def CudaCustomEnvGenerateObservation(
                    int32),
                   device=True)
 def CudaCustomEnvComputeReward(
-    rewards_arr,
-    loc_x_arr,
-    loc_y_arr,
-    kGridLength,
-    edge_hit_penalty_arr,
-    num_preys_arr,
-    agent_types_arr,
-    kEatingRewardForPredator,
-    kEatingPenaltyForPreys,
-    kEndOfGameRewardForPrey,
-    kEndOfGameRewardForPredator,
-    still_in_the_game_arr,
-    done_arr,
-    env_timestep_arr,
-    kNumAgents,
-    kEpisodeLength,
-    kEnvId,
-    kThisAgentId,
+        rewards_arr,
+        loc_x_arr,
+        loc_y_arr,
+        kGridLength,
+        edge_hit_penalty_arr,
+        num_preys_arr,
+        agent_types_arr,
+        kEatingRewardForPredator,
+        kEatingPenaltyForPreys,
+        kEndOfGameRewardForPrey,
+        kEndOfGameRewardForPredator,
+        still_in_the_game_arr,
+        done_arr,
+        env_timestep_arr,
+        kNumAgents,
+        kEpisodeLength,
+        kEnvId,
+        kThisAgentId,
 ):
     if kThisAgentId < kNumAgents:
         # Initialize rewards
@@ -200,13 +179,13 @@ def CudaCustomEnvComputeReward(
         numba_driver.syncthreads()
 
         min_dist = kGridLength * math.sqrt(2.0)
-        is_runner = not agent_types_arr[kThisAgentId]
+        is_prey = not agent_types_arr[kThisAgentId] # 0 for prey, 1 for predator
 
-        if is_runner and still_in_the_game_arr[kEnvId, kThisAgentId]:
+        if is_prey and still_in_the_game_arr[kEnvId, kThisAgentId]:
             for other_agent_id in range(kNumAgents):
-                is_tagger = agent_types_arr[other_agent_id] == 1
+                is_predator = agent_types_arr[other_agent_id] == 1
 
-                if is_tagger:
+                if is_predator:
                     dist = ComputeDistance(
                         loc_x_arr,
                         loc_y_arr,
@@ -217,12 +196,12 @@ def CudaCustomEnvComputeReward(
                     )
                     if dist < min_dist:
                         min_dist = dist
-                        nearest_tagger_id = other_agent_id
+                        nearest_predator_id = other_agent_id
 
-            if min_dist < 1: #TODO: need to change that 1
-                # The runner is tagged
+            if min_dist < 0.5:  # TODO: need to change that 1
+                # The prey us eaten
                 rewards_arr[kEnvId, kThisAgentId] += kEatingPenaltyForPreys
-                rewards_arr[kEnvId, nearest_tagger_id] += kEatingRewardForPredator
+                rewards_arr[kEnvId, nearest_predator_id] += kEatingRewardForPredator
 
                 # if kRunnerExitsGameAfterTagged: # TODO : Use to be kRunnerExitsGameAfterTagged but always true
                 still_in_the_game_arr[kEnvId, kThisAgentId] = 0
@@ -239,110 +218,120 @@ def CudaCustomEnvComputeReward(
             done_arr[kEnvId] = 1
 
 
-@numba_driver.jit((float32[:, ::1],
-                   float32[:, ::1],
-                   float32[:, ::1],
-                   float32[:, ::1],
-                   float32[:, ::1],
-                   int32[::1],
-                   float32,
-                   float32[::1],
-                   float32[::1],
-                   float32,
-                   float32,
-                   float32,
-                   float32,
-                   float32,
-                   int32[:, ::1],
-                   boolean,
-                   float32,
-                   float32,
-                   float32[:, :, ::1],
-                   int32[:, :, ::1],
-                   float32[:, ::1],
-                   int32[:, ::1],
-                   int32[::1],
-                   int32[::1],
-                   float32,
-                   float32,
-                   float32,
-                   float32,
-                   float32,
-                   int32[::1],
-                   int32[::1],
-                   int32,
-                   int32))
+@numba_driver.jit((float32[:, ::1],  # loc_x_arr
+                   float32[:, ::1],  # loc_y_arr
+                   float32[:, ::1],  # speed_arr
+                   float32[:, ::1],  # direction_arr
+                   float32[:, ::1],  # acceleration_arr
+                   int32[::1],  # agent_types_arr
+                   float32,  # kGridLength
+                   float32[::1],  # acceleration_actions_arr
+                   float32[::1],  # turn_actions_arr
+                   float32,  # kMaxSpeed
+                   float32,  # kMaxAcceleration
+                   float32,  # kMinAcceleration
+                   float32,  # kMaxTurn
+                   float32,  # kMinTurn
+                   int32[:, ::1],  # still_in_the_game_arr
+                   boolean,  # kUseFullObservation
+                   float32,  # kSeeingAngle
+                   float32,  # kSeeingDistance
+                   float32[:, :, ::1],  # obs_arr
+                   int32[:, :, ::1],  # action_indices_arr
+                   float32[:, ::1],  # edge_hit_penalty_arr
+                   int32[:, ::1],  # rewards_arr
+                   int32[::1],  # num_preys_arr
+                   int32[::1],  # num_predators_arr
+                   float32,  # kEdgeHitPenalty
+                   float32,  # kEatingRewardForPredator
+                   float32,  # kEatingPenaltyForPreys
+                   float32,  # kEndOfGameRewardForPrey
+                   float32,  # kEndOfGameRewardForPredator
+                   int32[::1],  # done_arr
+                   int32[::1],  # env_timestep_arr
+                   int32,  # kNumAgents
+                   int32))  # kEpisodeLength
 def NumbaCustomEnvStep(
-    loc_x_arr,
-    loc_y_arr,
-    speed_arr,
-    direction_arr,
-    acceleration_arr,
-    agent_types_arr,
-    kGridLength,
-    acceleration_actions_arr,
-    turn_actions_arr,
-    kMaxSpeed,
-    kMaxAcceleration,
-    kMinAcceleration,
-    kMaxTurn,
-    kMinTurn,
-    still_in_the_game_arr,
-    kUseFullObservation,
-    kSeeingAngle,
-    kSeeingDistance,
-    obs_arr,
-    action_indices_arr,
-    edge_hit_penalty_arr,
-    rewards_arr,
-    num_preys_arr,
-    num_predators_arr,
-    kEdgeHitPenalty,
-    kEatingRewardForPredator,
-    kEatingPenaltyForPreys,
-    kEndOfGameRewardForPrey,
-    kEndOfGameRewardForPredator,
-    done_arr,
-    env_timestep_arr,
-    kNumAgents,
-    kEpisodeLength,
+        loc_x_arr,
+        loc_y_arr,
+        speed_arr,
+        direction_arr,
+        acceleration_arr,
+        agent_types_arr,
+        kGridLength,
+        acceleration_actions_arr,
+        turn_actions_arr,
+        kMaxSpeed,
+        kMaxAcceleration,
+        kMinAcceleration,
+        kMaxTurn,
+        kMinTurn,
+        still_in_the_game_arr,
+        kUseFullObservation,
+        kSeeingAngle,
+        kSeeingDistance,
+        obs_arr,
+        action_indices_arr,
+        edge_hit_penalty_arr,
+        rewards_arr,
+        num_preys_arr,
+        num_predators_arr,
+        kEdgeHitPenalty,
+        kEatingRewardForPredator,
+        kEatingPenaltyForPreys,
+        kEndOfGameRewardForPrey,
+        kEndOfGameRewardForPredator,
+        done_arr,
+        env_timestep_arr,
+        kNumAgents,
+        kEpisodeLength,
 ):
     print("Numba step function call starting")
+    # Every block is an environment
+    # Every thread is an agent
     kEnvId = numba_driver.blockIdx.x
     kThisAgentId = numba_driver.threadIdx.x
+
+    # this is because the agent will take two actions : acceleration and turn
     kNumActions = 2
 
+    # At every call, each agent steps count is incremented
     if kThisAgentId == 0:
         env_timestep_arr[kEnvId] += 1
 
     numba_driver.syncthreads()
 
+    # We make sure that the timestep is valid
     assert env_timestep_arr[kEnvId] > 0 and env_timestep_arr[kEnvId] <= kEpisodeLength
 
     if kThisAgentId < kNumAgents:
+        # get the actions for this agent
         delta_acceleration = acceleration_actions_arr[action_indices_arr[kEnvId, kThisAgentId, 0]]
         delta_turn = turn_actions_arr[action_indices_arr[kEnvId, kThisAgentId, 1]]
 
+        # Update the agent's acceleration and directions
         acceleration_arr[kEnvId, kThisAgentId] += delta_acceleration
-        direction_arr[kEnvId, kThisAgentId] = ((direction_arr[kEnvId, kThisAgentId] + delta_turn) % kTwoPi) * still_in_the_game_arr[kEnvId, kThisAgentId]
+        direction_arr[kEnvId, kThisAgentId] = ((direction_arr[kEnvId, kThisAgentId] + delta_turn) % kTwoPi) * still_in_the_game_arr[
+            kEnvId, kThisAgentId]
 
+        # Direction is kept in [0, 2pi]
         if direction_arr[kEnvId, kThisAgentId] < 0:
             direction_arr[kEnvId, kThisAgentId] = (
-                kTwoPi + direction_arr[kEnvId, kThisAgentId]
+                    kTwoPi + direction_arr[kEnvId, kThisAgentId]
             )
 
         # Speed clipping
         speed_arr[kEnvId, kThisAgentId] = min(
             kMaxSpeed,
-            max(0.0, speed_arr[kEnvId, kThisAgentId] + acceleration_arr[kEnvId, kThisAgentId],)
+            max(0.0, speed_arr[kEnvId, kThisAgentId] + acceleration_arr[kEnvId, kThisAgentId], )
         ) * still_in_the_game_arr[kEnvId, kThisAgentId]
-
 
         # Reset acceleration to 0 when speed becomes 0 or
         # kMaxSpeed (multiplied by skill levels)
         if speed_arr[kEnvId, kThisAgentId] <= 0.0 or speed_arr[kEnvId, kThisAgentId] >= kMaxSpeed:
             acceleration_arr[kEnvId, kThisAgentId] = 0.0
 
+        # Update the agent's location
         loc_x_arr[kEnvId, kThisAgentId] += speed_arr[kEnvId, kThisAgentId] * math.cos(
             direction_arr[kEnvId, kThisAgentId]
         )
@@ -350,15 +339,16 @@ def NumbaCustomEnvStep(
             direction_arr[kEnvId, kThisAgentId]
         )
 
-        # Crossing the edge
+        # Check if the agent has crossed the edge
         has_crossed_edge = (
-            loc_x_arr[kEnvId, kThisAgentId] < 0
-            or loc_x_arr[kEnvId, kThisAgentId] > kGridLength
-            or loc_y_arr[kEnvId, kThisAgentId] < 0
-            or loc_y_arr[kEnvId, kThisAgentId] > kGridLength
+                loc_x_arr[kEnvId, kThisAgentId] < 0
+                or loc_x_arr[kEnvId, kThisAgentId] > kGridLength
+                or loc_y_arr[kEnvId, kThisAgentId] < 0
+                or loc_y_arr[kEnvId, kThisAgentId] > kGridLength
         )
 
         # Clip x and y if agent has crossed edge
+        # Here we should code a bounce back effect
         if has_crossed_edge:
             if loc_x_arr[kEnvId, kThisAgentId] < 0:
                 loc_x_arr[kEnvId, kThisAgentId] = 0.0
@@ -391,14 +381,13 @@ def NumbaCustomEnvStep(
         kMaxSpeed,
         still_in_the_game_arr,
         kUseFullObservation,
-        obs_arr,  
+        obs_arr,
         env_timestep_arr,
         kNumAgents,
         kEpisodeLength,
         kEnvId,
         kThisAgentId,
     )
-
 
     # --------------------------------
     # Compute reward                 -
